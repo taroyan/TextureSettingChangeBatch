52770e82188b7c3351041cb134b6c03c89759cb7
taroyan <name4yu@gmail.com>
2021-01-12 23:04:05 +0900

    wip

--- Assets/Editor/TextureImportChanges.cs
+++ Assets/Editor/TextureImportChanges.cs
@@ -6,16 +6,45 @@ using System.IO;
 
 public class TextureImportChanges : EditorWindow
 {
-
-	// コンボボックスのリスト
-	public static readonly string[] list = { "32", "64", "128", "256", "512", "1024","2048","4096","8192" };
-	
-	// コンボボックスのインデックス番号
-	public int index = 5;
-	
 	// Max Size
 	[Tooltip("32/64/128/256/512/1024/2048/4096/8192")]
 	public int MaxSize = 512;
+
+	// Mipmapを入れるかどうかのチェックボックス
+	public bool chkMipMap;
+
+	// Defaultのインポートセッティングを変更するかどうか
+	public bool chkDefault;
+
+	// Override for Standalone
+	public bool chkStandalone;
+
+	// Override for iOS
+	public bool chkiOS;
+
+	// Override for Android 
+	public bool chkAndroid;
+
+	// Override for WebGL
+	public bool chkWebGL;
+
+	// コンボボックスのリスト
+	public static readonly string[] list =
+	{
+		"32", "64", "128", "256", "512", "1024", "2048", "4096", "8192"
+	};
+
+	// コンボボックスのインデックス番号
+	public int index = 5;
+
+
+
+	// スクロール位置
+	private Vector2 _scrollPosition = Vector2.zero;
+
+	// ボタンサイズ
+	Vector2 buttonMinSize = new Vector2(100, 20);
+	Vector2 buttonMaxSize = new Vector2(1000, 50);
 
 
 	[MenuItem("Custom/TextureImportChanges")]
@@ -26,78 +55,109 @@ public class TextureImportChanges : EditorWindow
 
 	void OnGUI()
 	{
-
+		// 選択したコンボボックスのインデックスを取得
 		index = EditorGUILayout.Popup("最大画像サイズ", index, list);
 
+		// MipMapを入れるかどうか
+		chkMipMap = EditorGUILayout.Toggle("MipMap Enabled", chkMipMap);
 
-		EditorGUILayout.LabelField("現在選択中フォルダ", GetCurrentPath());
+		// Default Settings
+		chkDefault = EditorGUILayout.Toggle("Default", chkDefault);
 
-		if (GUILayout.Button("MaxSizeを一括変換する"))
+		// Override for Standalone
+		chkStandalone = EditorGUILayout.Toggle("Override for Standalone", chkStandalone);
+
+		// Override for iOS
+		chkiOS = EditorGUILayout.Toggle("Override for iOS", chkiOS);
+
+		// Override for Android
+		chkAndroid = EditorGUILayout.Toggle("Override for Android", chkAndroid);
+
+		// Override for WebGL
+		chkWebGL = EditorGUILayout.Toggle("Override for WebGL", chkWebGL);
+
+
+		//描画範囲が足りなければスクロール出来るように
+		_scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+
+		/*スクロールする描画処理*/
+		foreach (var path in GetCurrentPath())
 		{
-			Method2();
+			EditorGUILayout.LabelField("現在選択中フォルダ", path);
 		}
 
-		if (GUILayout.Button("一覧バックアップ"))
+		//スクロール箇所終了
+		EditorGUILayout.EndScrollView();
+
+
+		// ImortSettings一括処理
+		if (GUILayout.Button("ImportSettingsを一括変換する",
+			GUILayout.MinWidth(buttonMinSize.x), GUILayout.MinHeight(buttonMinSize.y),
+			GUILayout.MaxWidth(buttonMaxSize.x), GUILayout.MaxHeight(buttonMaxSize.y)))
 		{
-			Method1();
+			ChangeImportSettings();
 		}
-
-
 	}
 
-
-
-
 	// ファイル一覧を取得する(PNG/TIF/JPG/PSD/tga)
-	string[] getFileList(string path)
+	string[] getFileList(List<string> pathList)
 	{
-		if (!Directory.Exists(path)) return new string[0];
 
-		// PNG
-		string[] png = System.IO.Directory.GetFiles(
-			path, "*.png", System.IO.SearchOption.AllDirectories);
+		System.Collections.ArrayList listAll = new ArrayList(); // 全てのファイルArrayList
 
-		// TIF
-		string[] tif = System.IO.Directory.GetFiles(
-			path, "*.tif", System.IO.SearchOption.AllDirectories);
+		foreach (var path in pathList)
+		{
 
-		// TIFF
-		string[] tiff = System.IO.Directory.GetFiles(
-			path, "*.tiff", System.IO.SearchOption.AllDirectories);
+			if (!Directory.Exists(path)) return new string[0];
 
-		// JPG
-		string[] jpg = System.IO.Directory.GetFiles(
-			path, "*.jpg", System.IO.SearchOption.AllDirectories);
+			// PNG
+			string[] png = System.IO.Directory.GetFiles(
+				path, "*.png", System.IO.SearchOption.AllDirectories);
 
-		// JPEG
-		string[] jpeg = System.IO.Directory.GetFiles(
-			path, "*.jpeg", System.IO.SearchOption.AllDirectories);
-		
-		// PSD
-		string[] psd = System.IO.Directory.GetFiles(
-			path, "*.psd", System.IO.SearchOption.AllDirectories);
-		
-		// tga
-		string[] tga = System.IO.Directory.GetFiles(
-			path, "*.tga", System.IO.SearchOption.AllDirectories);
+			// TIF
+			string[] tif = System.IO.Directory.GetFiles(
+				path, "*.tif", System.IO.SearchOption.AllDirectories);
+
+			// TIFF
+			string[] tiff = System.IO.Directory.GetFiles(
+				path, "*.tiff", System.IO.SearchOption.AllDirectories);
+
+			// JPG
+			string[] jpg = System.IO.Directory.GetFiles(
+				path, "*.jpg", System.IO.SearchOption.AllDirectories);
+
+			// JPEG
+			string[] jpeg = System.IO.Directory.GetFiles(
+				path, "*.jpeg", System.IO.SearchOption.AllDirectories);
+
+			// PSD
+			string[] psd = System.IO.Directory.GetFiles(
+				path, "*.psd", System.IO.SearchOption.AllDirectories);
+
+			// tga
+			string[] tga = System.IO.Directory.GetFiles(
+				path, "*.tga", System.IO.SearchOption.AllDirectories);
 
 
-		System.Collections.ArrayList list1 = new System.Collections.ArrayList(png);
-		System.Collections.ArrayList list2 = new System.Collections.ArrayList(tif);
-		System.Collections.ArrayList list3 = new System.Collections.ArrayList(tiff);
-		System.Collections.ArrayList list4 = new System.Collections.ArrayList(jpg);
-		System.Collections.ArrayList list5 = new System.Collections.ArrayList(jpeg);
-		System.Collections.ArrayList list6 = new System.Collections.ArrayList(psd);
-		System.Collections.ArrayList list7 = new System.Collections.ArrayList(tga);
+			System.Collections.ArrayList list1 = new System.Collections.ArrayList(png);
+			System.Collections.ArrayList list2 = new System.Collections.ArrayList(tif);
+			System.Collections.ArrayList list3 = new System.Collections.ArrayList(tiff);
+			System.Collections.ArrayList list4 = new System.Collections.ArrayList(jpg);
+			System.Collections.ArrayList list5 = new System.Collections.ArrayList(jpeg);
+			System.Collections.ArrayList list6 = new System.Collections.ArrayList(psd);
+			System.Collections.ArrayList list7 = new System.Collections.ArrayList(tga);
 
-		list1.AddRange(list2);
-		list1.AddRange(list3);
-		list1.AddRange(list4);
-		list1.AddRange(list5);
-		list1.AddRange(list6);
-		list1.AddRange(list7);
+			listAll.AddRange(list1);
+			listAll.AddRange(list2);
+			listAll.AddRange(list3);
+			listAll.AddRange(list4);
+			listAll.AddRange(list5);
+			listAll.AddRange(list6);
+			listAll.AddRange(list7);
 
-		string[] files = (string[])list1.ToArray(typeof(string));
+		}
+
+		string[] files = (string[])listAll.ToArray(typeof(string));
 
 		// ファイル名を整形する
 		for (int i = 0; i < files.Length; i++)
@@ -109,35 +169,8 @@ public class TextureImportChanges : EditorWindow
 		return files;
 	}
 
-	//[ContextMenu("MaxSizeの一覧をバックアップする。")]
-	void Method1()
-	{
-		string[] files = getFileList(Application.dataPath + "/");
-		if (files.Length == 0)
-		{
-			Debug.LogWarning("画像ファイルが見つかりませんでした。");
-			return;
-		}
-
-		for (int i = 0; i < files.Length; i++)
-		{
-			files[i] = files[i].Replace("\\", "/");
-			files[i] = files[i].Replace(Application.dataPath, "Assets");
-
-			TextureImporter Ti = AssetImporter.GetAtPath(files[i]) as TextureImporter;
-			files[i] = Ti.maxTextureSize + "," + "\"" + files[i] + "\"";
-		}
-
-		// Assets/backup_all.txtを作成する
-		File.WriteAllText(Application.dataPath + "/backup_all.txt", string.Join("\n", files));
-		Debug.Log("Asset/backup_all.txtを作成しました。");
-
-		// エディタ側のファイル構成を更新する
-		AssetDatabase.Refresh();
-	}
-
-	//[ContextMenu("MaxSizeを一括変更する。")]
-	void Method2()
+	//[ContextMenu("ImportSettingsを一括変更する。")]
+	void ChangeImportSettings()
 	{
 		switch (this.index)
 		{
@@ -173,8 +206,6 @@ public class TextureImportChanges : EditorWindow
 				return;
 		}
 
-		// string[] files = getFileList(Application.dataPath + "/" + this.AssetsName);
-
 		string[] files = getFileList(GetCurrentPath()); // 現在選択中のフォルダ下のファイルを取得する
 
 		if (files.Length == 0)
@@ -192,8 +223,8 @@ public class TextureImportChanges : EditorWindow
 			// if (Ti.maxTextureSize > this.MaxSize)
 			// {
 
-			SetImportSettings(Ti);	// インポート設定変更
-			
+			SetImportSettings(Ti); // インポート設定変更
+
 			// 変更内容をプロジェクトに反映する ※この処理は数分かかります。                
 			// (注意)この3種のメソッドの使い方は正しくないかも知れません。
 			// (注意)とりあえず、MaxSizeを下げる分には動いているのでこのままにしておきます。
@@ -209,43 +240,27 @@ public class TextureImportChanges : EditorWindow
 	/// <summary>
 	/// 現在選択しているフォルダのパスを取得する
 	/// </summary>
-	private string GetCurrentPath()
+	private List<string> GetCurrentPath()
 	{
-		string rst = "";
-		
+		List<string> rstList = new List<string>();
+
 		// 選択したフォルダのパスを取得（複数選択した場合は初めの１つめだけ）
 		Object[] assets = Selection.GetFiltered(typeof(DefaultAsset), SelectionMode.Assets);
 
-		if (assets.Length == 1)	// １つだけ選択?(Yes) 現在複数選択は無効にしている。
+		foreach (var asset in assets)
 		{
-			if (assets[0] is DefaultAsset)
+			if (asset is DefaultAsset)
 			{
-				string path = AssetDatabase.GetAssetPath(assets[0]);
+				string path = AssetDatabase.GetAssetPath(asset);
 
 				if (AssetDatabase.IsValidFolder(path))
 				{
-					//Debug.Log("Valid folder. " + path);
-					rst = path;
+					rstList.Add(path);
 				}
 			}
 		}
-		
-		// 選択したフォルダ内のすべてのアセットを取ってくる。
-		// foreach (Object obj in Selection.GetFiltered(typeof(DefaultAsset), SelectionMode.DeepAssets))
-		// {
-		// 	if (obj is DefaultAsset)
-		// 	{
-		// 		string path = AssetDatabase.GetAssetPath(obj);
-		//
-		// 		if (AssetDatabase.IsValidFolder(path))
-		// 		{
-		// 			//Debug.Log("Valid folder. " + path);
-		// 			rst = path;
-		// 		}
-		// 	}
-		// }
 
-		return rst;
+		return rstList;
 	}
 
 	/// <summary>
@@ -255,52 +270,77 @@ public class TextureImportChanges : EditorWindow
 	{
 		// Spriteに設定
 		importer.textureType = TextureImporterType.Sprite;
-		
-		// MipMapのチェックを外す
-		importer.mipmapEnabled = false;
 
-		// TextureImporterPlatformSettings iPhone_png = new TextureImporterPlatformSettings ();
-		// iPhone_png.overridden = true;
-		// iPhone_png.name = "iPhone";
-		// iPhone_png.maxTextureSize = 2048;
-		// iPhone_png.format = TextureImporterFormat.ASTC_RGBA_4x4;
-		// iPhone_png.compressionQuality = 50;
-		// iPhone_png.allowsAlphaSplitting = false;
-		//
-		// TextureImporterPlatformSettings iPhone_jpeg = new TextureImporterPlatformSettings ();
-		// iPhone_jpeg.overridden = true;
-		// iPhone_jpeg.name = "iPhone";
-		// iPhone_jpeg.maxTextureSize = 2048;
-		// iPhone_jpeg.format = TextureImporterFormat.ASTC_RGB_4x4;
-		// iPhone_jpeg.compressionQuality = 50;
-		// iPhone_jpeg.allowsAlphaSplitting = false;
-		//
-		// TextureImporterPlatformSettings Android_png = new TextureImporterPlatformSettings ();
-		// Android_png.overridden = true;
-		// Android_png.name = "Android";
-		// Android_png.maxTextureSize = 2048;
-		// Android_png.format = TextureImporterFormat.DXT5;
-		// Android_png.compressionQuality = 50;
-		// Android_png.allowsAlphaSplitting = false;
-		//
-		// TextureImporterPlatformSettings Android_jpeg = new TextureImporterPlatformSettings ();
-		// Android_jpeg.overridden = true;
-		// Android_jpeg.name = "Android";
-		// Android_jpeg.maxTextureSize = 2048;
-		// Android_jpeg.format = TextureImporterFormat.DXT1;
-		// Android_jpeg.compressionQuality = 50;
-		// Android_jpeg.allowsAlphaSplitting = false;
-		
-		TextureImporterPlatformSettings WebGL_png = new TextureImporterPlatformSettings ();
-		WebGL_png.overridden = true;
+		// MipMapのチェック
+		importer.mipmapEnabled = chkMipMap;
+
+		if (chkDefault)
+		{
+			importer.maxTextureSize = MaxSize;
+		}
+
+		// Standalone
+		TextureImporterPlatformSettings Standalone_png = new TextureImporterPlatformSettings();
+		Standalone_png.overridden = chkStandalone;
+		Standalone_png.name = "Standalone";
+		Standalone_png.maxTextureSize = MaxSize;
+		Standalone_png.format = TextureImporterFormat.DXT5;
+		Standalone_png.compressionQuality = 50;
+		Standalone_png.allowsAlphaSplitting = false;
+
+		TextureImporterPlatformSettings Standalone_jpeg = new TextureImporterPlatformSettings();
+		Standalone_jpeg.overridden = chkStandalone;
+		Standalone_jpeg.name = "Standalone";
+		Standalone_jpeg.maxTextureSize = MaxSize;
+		Standalone_jpeg.format = TextureImporterFormat.DXT1;
+		Standalone_jpeg.compressionQuality = 50;
+		Standalone_jpeg.allowsAlphaSplitting = false;
+
+		// iOS
+		TextureImporterPlatformSettings iPhone_png = new TextureImporterPlatformSettings();
+		iPhone_png.overridden = chkiOS;
+		iPhone_png.name = "iPhone";
+		iPhone_png.maxTextureSize = MaxSize;
+		iPhone_png.format = TextureImporterFormat.ASTC_RGBA_4x4;
+		iPhone_png.compressionQuality = 50;
+		iPhone_png.allowsAlphaSplitting = false;
+
+		TextureImporterPlatformSettings iPhone_jpeg = new TextureImporterPlatformSettings();
+		iPhone_jpeg.overridden = chkiOS;
+		iPhone_jpeg.name = "iPhone";
+		iPhone_jpeg.maxTextureSize = MaxSize;
+		iPhone_jpeg.format = TextureImporterFormat.ASTC_RGB_4x4;
+		iPhone_jpeg.compressionQuality = 50;
+		iPhone_jpeg.allowsAlphaSplitting = false;
+
+		// Android
+		TextureImporterPlatformSettings Android_png = new TextureImporterPlatformSettings();
+		Android_png.overridden = chkAndroid;
+		Android_png.name = "Android";
+		Android_png.maxTextureSize = MaxSize;
+		Android_png.format = TextureImporterFormat.DXT5;
+		Android_png.compressionQuality = 50;
+		Android_png.allowsAlphaSplitting = false;
+
+		TextureImporterPlatformSettings Android_jpeg = new TextureImporterPlatformSettings();
+		Android_jpeg.overridden = chkAndroid;
+		Android_jpeg.name = "Android";
+		Android_jpeg.maxTextureSize = MaxSize;
+		Android_jpeg.format = TextureImporterFormat.DXT1;
+		Android_jpeg.compressionQuality = 50;
+		Android_jpeg.allowsAlphaSplitting = false;
+
+		// WebGL
+		TextureImporterPlatformSettings WebGL_png = new TextureImporterPlatformSettings();
+		WebGL_png.overridden = chkWebGL;
 		WebGL_png.name = "WebGL";
 		WebGL_png.maxTextureSize = MaxSize;
 		WebGL_png.format = TextureImporterFormat.DXT5;
 		// WebGL_png.compressionQuality = 50;
 		WebGL_png.allowsAlphaSplitting = false;
-		
-		TextureImporterPlatformSettings WebGL_jpeg = new TextureImporterPlatformSettings ();
-		WebGL_jpeg.overridden = true;
+
+		TextureImporterPlatformSettings WebGL_jpeg = new TextureImporterPlatformSettings();
+		WebGL_jpeg.overridden = chkWebGL;
 		WebGL_jpeg.name = "WebGL";
 		WebGL_jpeg.maxTextureSize = MaxSize;
 		WebGL_jpeg.format = TextureImporterFormat.DXT1;
@@ -308,16 +348,21 @@ public class TextureImportChanges : EditorWindow
 		WebGL_jpeg.allowsAlphaSplitting = false;
 
 
-		if (importer.DoesSourceTextureHaveAlpha ()) {
+		// ImportSettingをセットする
+		if (importer.DoesSourceTextureHaveAlpha())
+		{
 			//Alphaチャンネルがある場合
-			// importer.SetPlatformTextureSettings (iPhone_png);
-			// importer.SetPlatformTextureSettings (Android_png);
-			importer.SetPlatformTextureSettings (WebGL_png);
-
-		} else {
+			importer.SetPlatformTextureSettings(Standalone_png);
+			importer.SetPlatformTextureSettings(iPhone_png);
+			importer.SetPlatformTextureSettings(Android_png);
+			importer.SetPlatformTextureSettings(WebGL_png);
+		}
+		else
+		{
 			//Alphaチャンネルがない場合
-			// importer.SetPlatformTextureSettings (iPhone_jpeg);
-			// importer.SetPlatformTextureSettings (Android_jpeg);
+			importer.SetPlatformTextureSettings(Standalone_jpeg);
+			importer.SetPlatformTextureSettings(iPhone_jpeg);
+			importer.SetPlatformTextureSettings(Android_jpeg);
 			importer.SetPlatformTextureSettings(WebGL_jpeg);
 		}
 	}
